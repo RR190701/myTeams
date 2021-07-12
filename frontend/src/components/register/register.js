76c39b48c42c79e8b9f7fe87478fc5c9bff0af27
Rashika <rashikarawat01@gmail.com>
2021-07-12 13:40:34 +0530

    writing readMe

--- frontend/src/components/register/register.js
+++ frontend/src/components/register/register.js
@@ -168,7 +168,7 @@ export default function SignUp({history}) {
           </Button>
           <Grid container justifyContent="flex-end">
             <Grid item>
-              <Link href="/login" variant="body2">
+              <Link to = "/login" variant="body2">
                 Already have an account? Sign in
               </Link>
             </Grid>
